0cda6a39c5a413bef82bfbedff346cf1fbcc683a
Anmeet <anmeet619@yahoo.com>
2020-09-26 11:47:15 +1000

    final commit

---
+++ ticktock-backend/data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+    url: 'https://v16-web-newkey.tiktokcdn.com/1fe859672be45271e14af48adda2f9d0/5f6ed7c5/video/tos/useast2a/tos-useast2a-ve-0068c003/74e3d8bbc0054c788c1e31f33293a45f/?a=1988&br=3398&bt=1699&cr=0&cs=0&dr=0&ds=3&er=&l=2020092523550601018907203438118225&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=MzVocDw7PGt5dzMzZDczM0ApO2U7ZWQzPDxoNzU7NmdnZmdwajJuZi4uYjNfLS1jMTZzc2MuXmMyNjRfXjJhMl8yYTY6Yw%3D%3D&vl=&vr=',
+    channel: 'anita',
+    song: 'What is your name',
+    likes: '2350',
+    messages: '258',
+    description:' This is an example',
+    shares: '500'
+    },
+    {
+        url: 'https://v19-web-newkey.tiktokcdn.com/1ed938865bcb26d75c30956939611174/5f6ed70a/video/tos/useast2a/tos-useast2a-ve-0068/62e8973b4c564549b977875199ed0944/?a=1988&br=3594&bt=1797&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202009252351550101880612151B1154AF&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M3RrZ3l1bTd4bjMzMzczM0ApNWg2aDtmOmU5N2VlaTRmM2djLzMvbG4vZnFfLS1iMTZzc2I1YmIvXzIxNjYtMy4tY146Yw%3D%3D&vl=&vr=',
+        channel: 'anmeet23',
+        song: 'What is it',
+        likes: '550',
+        messages: '2258',
+        description:' This is an example',
+        shares: '300'  
+    },
+]
+
+module.exports = {data}
